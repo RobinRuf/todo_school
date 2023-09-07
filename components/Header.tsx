a30e944e26d265f11e647cec0e03246ef156b912
Robin <robin@macbookair.home>
2023-09-07 03:10:01 +0200

    fix(app): added a padding on the x-axis to prevent components of touching corners

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="flex w-screen max-w-4xl justify-between px-10 text-white pt-5">
       <p className="text-blue-400 font-light italic">ToDo - Dashboard</p>
-      <p>ToDo suchen (coming soon)</p>
+      <p className="hidden md:flex">ToDo suchen (coming soon)</p>
       <button
         className="text-white bg-blue-400/60 rounded-full px-4 py-1 hover:bg-blue-400/100 transition-transform transform hover:scale-110"
         onClick={() => signOut()}
